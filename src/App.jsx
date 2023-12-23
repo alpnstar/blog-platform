e360571d957af13f6916bd2751b1e37da4284cb8
Satiyadzhiev Daud Adjakavovich <81386387+alpnstar@users.noreply.github.com>
2023-12-23 22:56:31 +0300

    'commit'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./scss/style.scss";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation, useNavigate, useNavigation} from "react-router-dom";
 import NavBar from "./components/UI/NavBar/NavBar";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
@@ -8,15 +8,23 @@ import Error from "./pages/Error";
 import PostIdPage from "./pages/PostIdPage";
 
 const App = () => {
+    const location = useLocation();
+    const navigate = useNavigate('/About');
+
+    useEffect((() => {
+        location.pathname === '/' && navigate('/About');
+    }))
     return (
         <div>
                 <NavBar/>
-                <Routes>
-                    <Route path="/" element={<About/>}/>
-                    <Route path="/posts" element={<Posts/>}/>
-                    <Route path="/posts/:id" element={<PostIdPage/>}/>
-                    <Route path="/*" element={<Error/>}/>
-                </Routes>
+            <Routes>
+                <Route path="/About" element={<About/>}/>
+                <Route path="/posts" element={<Posts/>}/>
+                <Route path="/posts/:id" element={<PostIdPage/>}/>
+                <Route path="/*" element={<Error/>}/>
+
+            </Routes>
+
         </div>
     )
 }
--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -4,8 +4,6 @@ export const useSortedPosts = (posts, currentPage, selectedSort) => {
   return useMemo(() => {
     if (selectedSort) {
       return [...currentPage].sort((a, b) => {
-        console.log(a)
-        console.log(selectedSort)
         return a[selectedSort].localeCompare(b[selectedSort])
       });
     }
